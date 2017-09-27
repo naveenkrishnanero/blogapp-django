5bbe06cce801973a058007ecccd6b7d3d5c3d650
Naveen krishna Murakonda <mnaveenkrishna1566@gmail.com>
2017-09-27 14:37:27 -0400

    adding files of blogapp to the repository

---
+++ blog/blogapp/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+from django.utils.encoding import  python_2_unicode_compatible
+
+# Create your models here.
+@python_2_unicode_compatible
+
+class Post(models.Model):
+
+    author= models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(blank=True,null=True)
+    # blank = True implies data base validation allows null values to be true
+    # null = True implies data base stores empty values as NULL in the data base
+    
+
+
