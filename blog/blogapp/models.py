5b357cc44f0cbaceb6f1d141c85f43c43694fdc7
Naveen krishna Murakonda <mnaveenkrishna1566@gmail.com>
2017-09-27 15:26:32 -0400

    commit the file into repository

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -13,9 +13,20 @@ class Post(models.Model):
     author= models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(blank=True,null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True,null=True)
     # blank = True implies data base validation allows null values to be true
     # null = True implies data base stores empty values as NULL in the data base
-    
+
+
+    def publish(self):
+
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
 
 
